0aea3ebef92eefc547f017b284493d7478856e4f
Sunny <raringsunny@gmail.com>
2017-09-13 22:16:43 +0530

    initial commit

---
+++ PricingApp.js
@@ -0,0 +1,28 @@
+//Product class
+//Product data
+//1. enter ideal price of the products [enter product no. ]
+//2. show product information
+
+
+function product(name, id)
+{
+    this.productName = name;
+    this.productId = id;
+    this.productPrice = [];
+}
+
+[
+  {
+    "productId": "123",
+    "description": "iPhone",
+    "prices": [10, 20, 30],
+    "idealPrice": ""
+  },
+  {
+    "productId": "124",
+    "description": "new products",
+    "prices": [20, 10, 15],
+    "idealPrice": ""
+  }
+]
+
