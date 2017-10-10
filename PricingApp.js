bf1bb27fa2932cde9a294954d074c5ab75a22bf6
Sunny <raringsunny@gmail.com>
2017-10-10 14:56:43 +0530

    refactored to rename the readline object from rl to ask.

--- PricingApp.js
+++ PricingApp.js
@@ -3,6 +3,13 @@
 //1. enter ideal price of the products [enter product no. ]
 //2. show product information
 
+// ask for different prices for a product
+// user will tell a productId for which he wants to add the price
+// we will seek that product from the list of products
+// add the price against that product
+// addPriceForProduct(2, 20.00); // "productId not available"; if available then add the price to that product
+// Products.FindById(prodId)[0].enterIdealPrice(2, 20.00)
+
 function Product(name, id)
 {
     this.productName = name;
@@ -15,19 +22,11 @@ function Product(name, id)
     }
 }
 
-//menu();
-
 function menu() {
-    /*var choice = 5;
-    while(choice != 3) {
-        console.log("Enter your choice: ");
-        console.log("1. Add Product: ");
-        console.log("2. Add Price for a Product: ");
-        console.log("3. Exit");*/
-    
+
         const readline = require('readline');
         
-        const rl = readline.createInterface({
+        const ask = readline.createInterface({
         input: process.stdin,
         output: process.stdout
         });
@@ -36,12 +35,12 @@ function menu() {
 
         var recursiveAsyncReadLine = function(){
             
-            rl.question('Please select a product by entering Product Id: ', (selectedProduct) => {
+            ask.question('Please select a product by entering Product Id: ', (selectedProduct) => {
                     
                 if(selectedProduct == 0){
-                    rl.close();
+                    ask.close();
                 }
-                rl.question('Please enter ideal price for the selected product: ', (userIdealPrice) => {
+                ask.question('Please enter ideal price for the selected product: ', (userIdealPrice) => {
                         
                     //search for the selected product
                     const prod = findById(selectedProduct);
@@ -56,30 +55,13 @@ function menu() {
                     }
                     
                     recursiveAsyncReadLine();
-
+                    
                 });
-
             
             });
             
         };
-        
         recursiveAsyncReadLine();
-
-        // TODO: Log the answer in a database
-        // console.log(`Your have selected: ` + selectedProduct.productName);
-        
-        // const rl1 = readline.createInterface({
-        // input: process.stdin,
-        // output: process.stdout
-        // });
-
-        // rl1.question('Please enter ideal price for the selected Product ', (userIdealPrice) => {
-                
-        //         prod.addPriceForProduct(userIdealPrice);
-
-        // });
-
     }
 
 const products = [];
@@ -87,48 +69,21 @@ const products = [];
 main();
 
 function main() {
-   const sampleProducts =  [
-    {
-        "productId": "123",
-        "description": "iPhone",
-        "prices": [10, 20, 30],
-        "idealPrice": ""
-    },
-    {
-        "productId": "124",
-        "description": "new products",
-        "prices": [20, 10, 15],
-        "idealPrice": ""
-    }];
 
     const iPhone = addNewProduct("iPhone", 1);
     const android = addNewProduct("Samsung Galaxy", 2);
+
     products.push(iPhone);
     products.push(android);
 
-    listProducts();
+    listProducts(); //list products
     
-    getUserOption();
+    menu(); //menu listing the products and asking user to enter the product id
 
-    //console.log(products);
-
-    // ask for different prices for a product
-    // user will tell a productId for which he wants to add the price
-    // we will seek that product from the list of products
-    // add the price against that product
-    // addPriceForProduct(2, 20.00); // "productId not available"; if available then add the price to that product
-    // Products.FindById(prodId)[0].enterIdealPrice(2, 20.00)
-    // const prod = findById(2);
-    
-    //console.log(prod.productName);
 }
 
 function listProducts(){
     products.forEach(iterateProducts);
-}
-
-function getUserOption(){
-    menu();
 }
 
 function iterateProducts(item, index){
@@ -137,13 +92,8 @@ function iterateProducts(item, index){
 
 function findById(productId) {
     // return products.filter(p => p.productId == productId); //filter returns an array so changing it to use find method instead.
-    return products.find(p => p.productId == productId); 
+    return products.find(p => p.productId == productId);
 }
-
-//unused function
-/*function addPriceForProduct(userIdealPrice){
-    prod.enterIdealPrice(userIdealPrice);
-}*/
 
 function addNewProduct(productName, productId) {
     const product = new Product(productName, productId);
