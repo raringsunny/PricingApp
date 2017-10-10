0a8a3c24509caaf3121a1d021803db2bcc2b3a45
Sunny <raringsunny@gmail.com>
2017-10-10 19:07:07 +0530

    1. formatted code 2. done some refactoring 3. used arrow function for listProducts method 4. removed comments that were not required

--- PricingApp.js
+++ PricingApp.js
@@ -10,16 +10,17 @@
 // addPriceForProduct(2, 20.00); // "productId not available"; if available then add the price to that product
 // Products.FindById(prodId)[0].enterIdealPrice(2, 20.00)
 
-function Product(name, id)
-{
-    this.productName = name;
-    this.productId = id;
-    this.productPrice = []; //array to hold prices entered by different users.
+function Product(name, id) {
+  this.productName = name;
+  this.productId = id;
+  this.productPrice = []; //array to hold prices entered by different users.
 
-    this.enterIdealPrice = function(price){
-        this.productPrice.push(price) /*a method of product class to push the values to productPrice array.
+  this.enterIdealPrice = function(price) {
+    this.productPrice.push(
+      price
+    ); /*a method of product class to push the values to productPrice array.
                                      not sure if this is correct? */
-    }
+  };
 }
 
 const products = [];
@@ -27,68 +28,76 @@ const products = [];
 main();
 
 function main() {
+  const iPhone = addNewProduct("iPhone", 1);
+  const android = addNewProduct("Samsung Galaxy", 2);
 
-    const iPhone = addNewProduct("iPhone", 1);
-    const android = addNewProduct("Samsung Galaxy", 2);
+  products.push(iPhone);
+  products.push(android);
 
-    products.push(iPhone);
-    products.push(android);
+  listProducts(); //list products
 
-    listProducts(); //list products
-    
-    menu(); //user gets a prompt to select a product to which the ideal price has to be added
+  menu(); //user gets a prompt to select a product to which the ideal price has to be added
 }
 
-function listProducts(){
-    products.forEach(item => console.log("Product Id: " + item.productId + "\t Product name: " + item.productName));
+function listProducts() {
+  products.forEach(item =>
+    console.log(
+      "Product Id: " + item.productId + "\t Product name: " + item.productName
+    )
+  );
 }
 
 function findById(productId) {
-    // return products.filter(p => p.productId == productId); //filter returns an array so changing it to use find method instead.
-    return products.find(p => p.productId == productId);
+  // return products.filter(p => p.productId == productId); //filter returns an array so changing it to use find method instead.
+  return products.find(p => p.productId == productId);
 }
 
 function addNewProduct(productName, productId) {
-    const product = new Product(productName, productId);
-    return product;
+  const product = new Product(productName, productId);
+  return product;
 }
 
 function menu() {
+  const readline = require("readline");
 
-        const readline = require('readline');
-        
-        const ask = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-        });
-        
-        var selectedProduct = -1;
+  const ask = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-        var recursiveAsyncReadLine = function(){
-            
-            ask.question('Please select a product by entering Product Id: ', (selectedProduct) => {
-                
-                if(selectedProduct == 0){
-                    ask.close();
-                    return;
-                }
+  var selectedProduct = -1;
 
-                ask.question('Please enter ideal price for the selected product: ', (userIdealPrice) => {
-                    
-                    //search for the selected product
-                    const prod = findById(selectedProduct);
+  var recursiveAsyncReadLine = function() {
+    ask.question(
+      "Please select a product by entering Product Id: ",
+      selectedProduct => {
+        if (selectedProduct == 0) {
+          ask.close();
+          return;
+        }
 
-                    if (prod == null){
-                        console.log("Product not found.");
-                    }
-                    else{
-                    prod.enterIdealPrice(userIdealPrice);
-                    console.log(prod.productPrice);
-                    console.log("Product Ideal price added successfully.");   
-                    }
-                    recursiveAsyncReadLine();
-                }); 
-            });
-        };
-        recursiveAsyncReadLine();
-    }
+        ask.question(
+          "Please enter ideal price for the selected product: ",
+          userIdealPrice => {
+            //search for the selected product
+            const prod = findById(selectedProduct);
+
+            if (prod == null) {
+              console.log("Product not found.");
+            } else {
+              prod.enterIdealPrice(userIdealPrice);
+              console.log(prod.productPrice);
+              console.log("Product Ideal price added successfully.");
+            }
+            recursiveAsyncReadLine();
+          }
+        );
+      }
+    );
+  };
+  recursiveAsyncReadLine();
+}
+
+module.exports = {
+	addNewProduct: addNewProduct
+}
